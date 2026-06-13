fde4e3ac7f3b6f6a2d49b6850aa05b5c28fc9ed6
Tanishsnghhh <142775471+Tanishsnghhh@users.noreply.github.com>
2026-06-13 20:12:27 +0530

    Add Cypress API tests and Selenium updates

---
+++ selenium-jenkins-testing/src/test/java/com/uspeek/tests/LicensesApiTest.java
@@ -0,0 +1,34 @@
+package com.uspeek.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LicensesApiTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+    
+    @Test
+    public void testLicensesEndpoint() throws Exception {
+        driver.get("http://localhost:3000/api/licenses");
+        Thread.sleep(2000);
+        
+        String pageSource = driver.getPageSource();
+        System.out.println("Licenses Source: " + pageSource.length() + " chars");
+        Assert.assertNotNull(pageSource, "Page source should not be null");
+    }
+    
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
